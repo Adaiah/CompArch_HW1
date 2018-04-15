384ae4779080076f9f9d297f963e0f2b7bd0e07a
tom.norman@campus.technion.ac.il <sometom@gmail.com>
2018-04-15 21:42:57 +0300

    order problem
    the order of the memcpy ist good

--- sim_core.cpp
+++ sim_core.cpp
@@ -4,8 +4,7 @@
 #include "sim_api.h"
 static SIM_coreState mainCoreState;
 static int32_t EX_ans[2] = {0,0}, MEM_ans[2] = {0,0}, WB_ans[2] = {0,0};
-static int stages_filled = 0;
-static int32_t stage_pc[SIM_PIPELINE_DEPTH] = {0};
+static int32_t stage_pc[SIM_PIPELINE_DEPTH] = {0}; // pc of the current cmd in a stage + 4
 
 //0 - new command output
 //1 - old command output
@@ -19,8 +18,12 @@ static int32_t stage_pc[SIM_PIPELINE_DEPTH] = {0};
   \returns 0 on success. <0 in case of initialization failure.
 */
 int SIM_CoreReset(void) {
-	if(memset(mainCoreState, 0, sizeof(mainCoreState)))	return 0;
-	else return -1;
+	if(memset(mainCoreState, 0, sizeof(mainCoreState))) {
+		return 0;
+	}
+	else {
+		return -1;
+	}
 }
 
 /*! SIM_CoreClkTick: Update the core simulator's state given one clock cycle.
@@ -32,47 +35,47 @@ void SIM_CoreClkTick() {
 	SIM_cmd ex_pipe;
 	SIM_cmd mem_pipe;
 	SIM_cmd wb_pipe;
-	memset(if_pipe, mainCoreState.pipeStageState[0].cmd, sizeof(SIM_cmd));
-	memset(id_pipe, mainCoreState.pipeStageState[1].cmd, sizeof(SIM_cmd));
-	memset(ex_pipe, mainCoreState.pipeStageState[2].cmd, sizeof(SIM_cmd));
-	memset(mem_pipe, mainCoreState.pipeStageState[3].cmd, sizeof(SIM_cmd));
-	memset(wb_pipe, mainCoreState.pipeStageState[4].cmd, sizeof(SIM_cmd));
+	memcpy(if_pipe, mainCoreState.pipeStageState[0].cmd, sizeof(SIM_cmd));
+	memcpy(id_pipe, mainCoreState.pipeStageState[1].cmd, sizeof(SIM_cmd));
+	memcpy(ex_pipe, mainCoreState.pipeStageState[2].cmd, sizeof(SIM_cmd));
+	memcpy(mem_pipe, mainCoreState.pipeStageState[3].cmd, sizeof(SIM_cmd));
+	memcpy(wb_pipe, mainCoreState.pipeStageState[4].cmd, sizeof(SIM_cmd));
 	if( !split_regfile && !forwarding ) {//when we start the machine nothing is initialized!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 										 // should also do something with src1Val and src2Val!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		// IF 
 		SIM_MemInstRead(mainCoreState.pc, mainCoreState.pipeStageState[0].cmd);
 		mainCoreState.pc += 4;
-		stage_pc[stages_filled % SIM_PIPELINE_DEPTH] = mainCoreState.pc;
+		stages_filled[FETCH] = mainCoreState.pc;
 		// ID
-		if(stages_filled >= 1) {
-			memset(mainCoreState.pipeStageState[1].cmd, if_pipe, sizeof(SIM_cmd));
+		if(stages_filled[DECODE]) {
+			memcpy(mainCoreState.pipeStageState[1].cmd, if_pipe, sizeof(SIM_cmd));
 		}
 		// EX
-		if(stages_filled >= 2) {
+		if(stages_filled[EXECUTE]) {
 			EX_ans[1] = EX_ans[0];
-			memset(mainCoreState.pipeStageState[2].cmd, id_pipe, sizeof(SIM_cmd));
+			memcpy(mainCoreState.pipeStageState[2].cmd, id_pipe, sizeof(SIM_cmd));
 			SIM_cmd_opcode opc = id_pipe.opcode;
 			if(opc == CMD_ADD || opc == CMD_SUB || opc == CMD_ADDI || opc == CMD_SUBI) {
-				EX_ans[0] = makeArith(id_pipe.cmd.src1, id_pipe.cmd.src2, id_pipe.cmd.isSrc2Imm, opc);
+				EX_ans[0] = makeArith(id_pipe.src1, id_pipe.src2, id_pipe.isSrc2Imm, opc);
 			}
 			else if(opc == CMD_LOAD) {
-				EX_ans[0] = makeArith(id_pipe.cmd.src1, id_pipe.cmd.src2, id_pipe.cmd.isSrc2Imm, opc);
+				EX_ans[0] = makeArith(id_pipe.src1, id_pipe.src2, id_pipe.isSrc2Imm, opc);
 			}
 			else if(opc == CMD_STORE) {
-				EX_ans[0] = makeArith(id_pipe.cmd.dst, id_pipe.cmd.src2, id_pipe.cmd.isSrc2Imm, opc);
+				EX_ans[0] = makeArith(id_pipe.dst, id_pipe.src2, id_pipe.isSrc2Imm, opc);
 			}
 			else if(opc == CMD_BR || opc == CMD_BREQ || opc == CMD_BRNEQ) {
-				EX_ans[0] = (id_pipe.cmd.src1 == id_pipe.cmd.src2) ? 1 : 0;
+				EX_ans[0] = (id_pipe.src1 == id_pipe.src2) ? 1 : 0;
 			}
 		}
 		// MEM
-		if(stages_filled >= 3) {
+		if(stages_filled[MEMORY]) {
 			MEM_ans[1] = MEM_ans[0];
-			memset(mainCoreState.pipeStageState[3].cmd, ex_pipe, sizeof(SIM_cmd));\
+			memcpy(mainCoreState.pipeStageState[3].cmd, ex_pipe, sizeof(SIM_cmd));\
 			MEM_ans[0] = EX_ans[1];
 			SIM_cmd_opcode opc = ex_pipe.opcode;
 			if(opc == CMD_STORE) {
-					SIM_MemDataWrite(MEM_ans[0], ex_pipe.cmd.src1);
+					SIM_MemDataWrite(MEM_ans[0], ex_pipe.src1);
 			}
 			else if(opc == CMD_LOAD) {
 				if(!SIM_MemDataRead(MEM_ans[0], MEM_ans))
@@ -80,22 +83,31 @@ void SIM_CoreClkTick() {
 					;
 			}
 			else if(opc == CMD_BR || (opc == CMD_BREQ && MEM_ans[0] == 1) || (opc == CMD_BRNEQ && MEM_ans[0] == 0)) {
-				mainCoreState.pc = stage_pc[MEMORY]
+				mainCoreState.pc = stages_filled[MEMORY] + ex_pipe.dst; // the next command is the answer from the EX stage
+				memcpy(mainCoreState.pipeStageState[FETCH].cmd, 0, sizeof(SIM_cmd));
+				memcpy(mainCoreState.pipeStageState[DECODE].cmd, 0, sizeof(SIM_cmd));
+				memcpy(mainCoreState.pipeStageState[EXECUTE].cmd, 0, sizeof(SIM_cmd));
+				stages_filled[FETCH] = 0;
+				stages_filled[DECODE] = 0;
+				stages_filled[EXECUTE] = 0;
 			}
 		}
 		// WB
-		if(stages_filled >= 4) {
+		if(stages_filled[WRITEBACK]) {
 			WB_ans[1] = WB_ans[0];
-			SIM_cmd_opcode opc = wb_pipe.cmd.opcode;
-			if(wb_pipe.cmd.dst != 0 && (opc == CMD_ADD || opc == CMD_SUB || opc == CMD_ADDI || opc == CMD_SUBI || opc == CMD_LOAD)) {
-				curState.regFile[wb_pipe.cmd.dst] = WB_ans[1];
+			SIM_cmd_opcode opc = wb_pipe.opcode;
+			if(wb_pipe.dst != 0 && (opc == CMD_ADD || opc == CMD_SUB || opc == CMD_ADDI || opc == CMD_SUBI || opc == CMD_LOAD)) {
+				curState.regFile[wb_pipe.dst] = WB_ans[1];
 			}
-			memset(mainCoreState.pipeStageState[4].cmd, mem_pipe, sizeof(SIM_cmd));
+			memcpy(mainCoreState.pipeStageState[4].cmd, mem_pipe, sizeof(SIM_cmd));
 			WB_ans[0] = MEM_ans[1];
 		}
 
 		//
-		++stages_filled;
+		stages_filled[WRITEBACK] = stages_filled[MEMORY];
+		stages_filled[MEMORY] = stages_filled[EXECUTE];
+		stages_filled[EXECUTE] = stages_filled[DECODE];
+		stages_filled[DECODE] = stages_filled[FETCH];
 	}
 }
 
